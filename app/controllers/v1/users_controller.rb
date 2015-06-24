01fee212dbacfbd48b2ae20616e2c20cf210aae1
Anil Maurya <anil@joshsoftware.com>
2015-06-24 23:41:41 +0530

    Remove unused join_group action from users_controller [cleanup]

--- app/controllers/v1/users_controller.rb
+++ app/controllers/v1/users_controller.rb
@@ -1,20 +1,8 @@
 module V1
   class UsersController < ApiController
-    before_action :load_group, only: [:join_group]
-
+    
     def show
       render json: current_user
-    end
-
-    def join_group
-      @group.user_ids << current_user.id
-      render json: @group.save
-    end
-
-    private
-
-    def load_group
-      @group = Group.find(params[:id])
     end
 
   end
